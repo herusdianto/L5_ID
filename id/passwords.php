a3c9a9ce77eebe0cfcdcb215d2c4d88dd1a218cf
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2015-04-14 21:11:19 +0700

    initial commit

---
+++ id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "Password minimal harus 6 karakter dan sama dengan konfirmasi.",
+    "user"     => "Pengguna dengan email tersebut tidak ditemukan.",
+    "token"    => "Token reset password tidak valid.",
+    "sent"     => "Pengingat password telah dikirim ke email anda!",
+    "reset"    => "Password berhasil direset!",
+
+];
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Laravel 5 Indonesia Translation
+
+Laravel 5 validasi, password & reminder bahasa Indonesia.
